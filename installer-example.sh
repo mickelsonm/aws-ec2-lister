0101cb440e8097d7c776980e7cafdbe140cf0306
Matt Mickelson <mattmickelson@TruSignals-MacBook-Pro.local>
2016-02-14 00:38:27 -0600

    updates the installer script example

--- installer-example.sh
+++ installer-example.sh
@@ -2,9 +2,21 @@
 
 LISTER_PATH=$HOME"/Downloads/darwin-ec2-lister"
 
+# handle optional arguments
+for arg in "$@"; do
+  case $arg in
+    "--latest" )
+    echo "Grabbing the latest of the lister";
+    if [ -f "$LISTER_PATH" ]; then
+      rm $LISTER_PATH;
+    fi
+  esac
+done
+
+# we can use the lister if it already is where it should be
 if [ -f "$LISTER_PATH" ]; then
   aws ec2 describe-instances | $LISTER_PATH;
-else
+else # we configure it
   echo "Configuring the lister...";
   aws s3 cp s3://my-executables/darwin-ec2-lister $LISTER_PATH;
   chmod +x $LISTER_PATH;
